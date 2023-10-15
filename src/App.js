39c2fa3179bf8a163ac58cd85e1e529f2499e859
Sahishma <sahishmasahadevan@gmail.com>
2023-10-15 17:39:50 +0530

    complete essential features

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
-import React from 'react';
-import NavBar from './Components/NavBar/NavBar';
-import Banner from './Components/Banner/Banner';
-import RowPost from './Components/RowPost/RowPost';
-import { Originals , action } from './Urls';
-
+import "./App.css";
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+import { Originals, action } from "./Urls";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
-      <RowPost url={Originals} title='Netflix Originals'  />
-      <RowPost url={action} title='Action' isSmall  />
+      <RowPost url={Originals} title="Netflix Originals" />
+      <RowPost url={action} title="Action" isSmall />
     </div>
   );
 }
